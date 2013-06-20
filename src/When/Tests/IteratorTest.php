28ce2fb9f47d7f92dea34eddbd0931553c07ee77
Ryan Kadwell <ryan@pause.ca>
2013-06-20 12:17:44 -0400

    Changed structure to support PSR-0 style autoloading Update styling to PSR-2 standards Added composer.json Added phpunit.xml to root for running tests

---
+++ src/When/Tests/IteratorTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace When\Tests;
+
+use When\When;
+use When\Iterator;
+use DateTime;
+
+class IteratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testDateWithoutCache()
+    {
+        $results[] = new DateTime('1997-09-29 09:00:00');
+        $results[] = new DateTime('1997-10-30 09:00:00');
+        $results[] = new DateTime('1997-11-27 09:00:00');
+        $results[] = new DateTime('1997-12-30 09:00:00');
+        $results[] = new DateTime('1998-01-29 09:00:00');
+        $results[] = new DateTime('1998-02-26 09:00:00');
+        $results[] = new DateTime('1998-03-30 09:00:00');
+
+        $r = new Iterator();
+        $r->recur('19970929T090000', 'monthly')->count(7)->byday(array('MO', 'TU', 'WE', 'TH', 'FR'))->bysetpos(array(-2));
+
+        $counter = 0;
+        foreach ($r as $result) {
+            $this->assertEquals($result, $results[$counter]);
+            $counter++;
+        }
+
+        // if we rewind does it still work?
+        $r->rewind();
+
+        $counter = 0;
+        foreach ($r as $result) {
+            $this->assertEquals($result, $results[$counter]);
+            $counter++;
+        }
+    }
+
+    public function testDateWithCache()
+    {
+        $results[] = new DateTime('1997-09-29 09:00:00');
+        $results[] = new DateTime('1997-10-30 09:00:00');
+        $results[] = new DateTime('1997-11-27 09:00:00');
+        $results[] = new DateTime('1997-12-30 09:00:00');
+        $results[] = new DateTime('1998-01-29 09:00:00');
+        $results[] = new DateTime('1998-02-26 09:00:00');
+        $results[] = new DateTime('1998-03-30 09:00:00');
+
+        $r = new Iterator(true);
+        $r->recur('19970929T090000', 'monthly')->count(7)->byday(array('MO', 'TU', 'WE', 'TH', 'FR'))->bysetpos(array(-2));
+
+        $counter = 0;
+        foreach ($r as $result) {
+            $this->assertEquals($result, $results[$counter]);
+            $counter++;
+        }
+
+        // if we rewind does it still work?
+        $r->rewind();
+
+        $counter = 0;
+        foreach ($r as $result) {
+            $this->assertEquals($result, $results[$counter]);
+            $counter++;
+        }
+    }
+
+}
